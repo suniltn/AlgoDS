16d89713b63f24102d7f741227776b63d44c04bb
suniltn <suniltn.sit@gmail.com>
2017-02-07 21:03:40 -0800

    test3

---
+++ src/string/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Created by stipturn on 2/7/17.
+ */
+
+package string;
+
+public class HelloWorld {
+
+public static void main (String[] args) {
+
+    System.out.println("Hello World");
+}
+}
